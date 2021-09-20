55792a3bdc81ca05c2330feb43ecb16a5081f871
ledbit <ec2-user@ip-10-3-60-177.us-west-2.compute.internal>
2021-09-20 00:04:38 +0000

    reorg cases

--- run.sh
+++ run.sh
@@ -13,30 +13,39 @@ function run_bench() {
   echo waiting for source container to exit $SRC_CONTAINER 1>&2
   docker wait $SRC_CONTAINER 1>&2
   
-  TIME_SPENT_SECS=$(cat /sys/fs/cgroup/cpu/docker/$TEST_CONTAINER/cpuacct.stat | awk '{ sum += $2 } END { print sum/100 }')
-  echo $TIME_SPENT_SECS
+  CPU_SECS=$(cat /sys/fs/cgroup/cpu/docker/$TEST_CONTAINER/cpuacct.stat | awk '{ sum += $2 } END { print sum/100 }')
+  MEM_RSS=$(cat /sys/fs/cgroup/memory/docker/$TEST_CONTAINER/memory.stat | grep '^rss ' | awk '{print $2}')
+
+  echo "$CPU_SECS,$MEM_RSS"
 }
 
 CRIBL_VERSION="3.1.2-TC4"
 VECTOR_VERSION="0.16.1"
 FLUENTBIT_VERSION="1.8.7"
-
 export CRIBL_VERSION
 export VECTOR_VERSION
 export FLUENTBIT_VERSION
 
-RESULTS="DIR,CPU_TIME"
-for file in $(find cases/ -name docker-compose.yml)
+RESULT_FILE="results/$(date +"%Y%m%dT%H%M%S")/results.csv"
+mkdir -p $(dirname $RESULT_FILE)
+
+
+echo "Results path=${RESULT_FILE}"
+echo "DIR,CPU_TIME,MEM_RSS" > $RESULT_FILE
+
+# find and run all the tests, order by the system being tested (smoothen out docker pulls)
+for file in $(find cases/ -name docker-compose.yml | sort -t/ -k3 -k2)
 do
   DIR=$(dirname $file)
   echo -e "\n\nEntering $DIR ..." 1>&2
   cd $DIR
-  TIME=$(run_bench)
+  RUN_STATS=$(run_bench)
   clean_bench
   cd -
-  echo ">> RESULTS: $DIR,$TIME" 1>&2
-  RESULTS="$RESULTS\n$DIR,$TIME"
+  echo "${DIR#cases/},$RUN_STATS" >> $RESULT_FILE
+  RESULTS="$RESULTS\n$DIR,$RUN_STATS"
 done
 
 echo -e "\n\n######## RESULTS ###########"
-echo -e $RESULTS
+cat $RESULT_FILE
+
