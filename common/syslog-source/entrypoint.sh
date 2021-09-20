29b7214bcb130560d736741cb5baa58b698158ff
ledbit <ec2-user@ip-10-3-60-177.us-west-2.compute.internal>
2021-09-20 05:36:16 +0000

    wait for dest system to come up

--- common/syslog-source/entrypoint.sh
+++ common/syslog-source/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-sleep 10
-zcat /data/syslog.log.gz | nc -q0 test-system 9514
-zcat /data/syslog.log.gz | nc -q0 test-system 9514
-zcat /data/syslog.log.gz | nc -q0 test-system 9514
+PORT=9514 
+
+echo "waiting for test-system to launch and open up port: $PORT"
+while ! nc -z test-system $PORT; do   
+  sleep 1 # wait for 1 second before check again
+done
+
+echo "test-system launched, port $PORT is now accessible ..."
+
+zcat /data/syslog.log.gz | nc -q0 test-system $PORT
+zcat /data/syslog.log.gz | nc -q0 test-system $PORT
+zcat /data/syslog.log.gz | nc -q0 test-system $PORT
